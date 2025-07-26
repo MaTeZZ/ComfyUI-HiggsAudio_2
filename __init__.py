1e6a6ca738b26bc262221b219af642f35bd6c5b6
MaTeZZ <martin@beeon.net>
2025-07-26 23:14:50 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,25 +1,3 @@
-from .nodes import (
-    LoadHiggsAudioModel, 
-    LoadHiggsAudioTokenizer, 
-    LoadHiggsAudioSystemPrompt, 
-    LoadHiggsAudioPrompt, 
-    HiggsAudio
-)
-
-NODE_CLASS_MAPPINGS = {
-    "LoadHiggsAudioModel": LoadHiggsAudioModel,
-    "LoadHiggsAudioTokenizer": LoadHiggsAudioTokenizer,
-    "LoadHiggsAudioSystemPrompt": LoadHiggsAudioSystemPrompt,
-    "LoadHiggsAudioPrompt": LoadHiggsAudioPrompt,
-    "HiggsAudio": HiggsAudio,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "LoadHiggsAudioModel": "Load Higgs Audio Model",
-    "LoadHiggsAudioTokenizer": "Load Higgs Audio Tokenizer",
-    "LoadHiggsAudioSystemPrompt": "Load Higgs Audio System Prompt",
-    "LoadHiggsAudioPrompt": "Load Higgs Audio Prompt",
-    "HiggsAudio": "Higgs Audio wrapper Generator",
-} 
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
